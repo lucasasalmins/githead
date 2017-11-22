fe02aa09729d933890920d454dec69f77c9d88b6
Jon Sanders <jonsanders101@aol.com>
2017-11-22 12:12:24 +0000

    Add init function which logs to console

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-module.exports = {
-  testString : "my test string"
-};
 console.log("You launched githead!");
+
+function init () {
+  console.log("Helpful message about how git works");
+}
+
+module.exports = {
+  testString : "my test string",
+  init : init
+};
--- test/testIndex.js
+++ test/testIndex.js
@@ -1,8 +1,17 @@
 var assert = require('assert');
 var index = require('../index.js');
+var sinon = require('sinon');
 
 describe('.testString', function () {
   it('returns "my test string"', function () {
     assert.equal(index.testString, "my test string");
+  });
+});
+describe('.init', function () {
+  it('returns a helpful message about "git init" to the command line', function () {
+    var consoleSpy = sinon.spy(console, "log");
+    index.init();
+    assert(consoleSpy.calledWith("Helpful message about how git works"));
+    console.log.restore();
   });
 });
